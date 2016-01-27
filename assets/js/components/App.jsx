0bcd39808b83046bba9f31421e4a19153ed609b2
Kabir <kabeer182010@gmail.com>
2016-01-28 01:45:14 +0545

    Simplify level 3

--- assets/js/components/App.jsx
+++ assets/js/components/App.jsx
@@ -3,11 +3,23 @@ import NoteList from './NoteList.jsx';
 import NoteForm from './NoteForm.jsx';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {id: 1, title: 'Foo', text: 'This is just a test note.'},
+                {id: 2, title: 'Bar', text: 'This is just another test note.'}
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
-                <NoteList />
+                <NoteList items={this.state.notes} />
                 <NoteForm />
             </div>
         );
--- assets/js/components/NoteList.jsx
+++ assets/js/components/NoteList.jsx
@@ -3,37 +3,26 @@ import Note from './Note.jsx';
 
 class NoteList extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            notes: [
-                {id: 1, title: 'Foo', text: 'This is just a test note.'},
-                {id: 2, title: 'Bar', text: 'This is just another test note.'}
-            ]
-        };
-    }
-
-    hasNotes() {
-        return (this.state.notes.length > 0 );
-    }
-
     render() {
         // If there are notes then render the List view
         // Else show the message
         return (
             <section className="note-list-wrapper">
-                {this.hasNotes() ? listView(this.state.notes) : emptyMessage()}
+                {this.hasNotes() ? listView(this.props.items) : emptyMessage()}
             </section>
         );
     }
+
+    hasNotes() {
+        return (this.props.items.length > 0 );
+    }
 }
 
-function listView(list) {
+function listView(items) {
 
     return (
         <div className="note-list list-group">
-            {list.map(item => (
+            {items.map(item => (
                 <Note key={item.id} title={item.title} text={item.text} />
             ))}
         </div>
@@ -41,9 +30,7 @@ function listView(list) {
 }
 
 function emptyMessage() {
-    return (
-        <div className="alert alert-warning">No Notes available</div>
-    );
+    return <div className="alert alert-warning">No Notes available</div>;
 }
 
 export default NoteList;
